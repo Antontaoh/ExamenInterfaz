8bb01d9c6647929c72b0aaa2e97ec489ccef82f1
Antonio Pérez Anglada <antoniopereznaglada.correo@gmail.com>
2025-03-10 11:25:15 +0100

    added first step

---
+++ main/src/test/java/examen_interfaz/main/ExamenInterfacesApplicationTests.java
@@ -0,0 +1,13 @@
+package examen_interfaz.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenInterfacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
